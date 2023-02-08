657657997b665c90103153bb593f0d759f1beffc
reccard93 <riccardo.ciurlino@gmail.com>
2023-02-08 12:12:57 +0100

    inserito deploy automatico azure\github

--- MyClassLibrary/Data/ToDoListDbContext.cs
+++ MyClassLibrary/Data/ToDoListDbContext.cs
@@ -6,18 +6,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 
 namespace MyClassLibrary.Data
 {
     public class ToDoListDbContext : DbContext
     {
+        private readonly IConfiguration _config;
         public ToDoListDbContext()
         {
+
+        }
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options, IConfiguration config)
+            : base(options)
+        {
+            _config = config;
         }
 
-        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options) : base(options) { }
         public DbSet<ToDoList> ToDoList { get; set; } = null!;
         public DbSet<Dipendenti> Dipendenti { get; set; } = null!;
+
+        private static string GetConnectionStringFromKeyVault(IConfiguration config)
+        {
+            try
+            {
+                string keyVaultUrl = config["KeyVaultUrl"];
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                        azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var secret = keyVaultClient.GetSecretAsync(keyVaultUrl, "ConnectionStringa").GetAwaiter().GetResult();
+                return secret.Value;
+            }
+            catch (Exception ex)
+            {
+                // Logga l'eccezione o gestiscila come desideri
+                Console.WriteLine("Errore durante la recupero della connection string dal Key Vault: " + ex.Message);
+                throw;
+            }
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = GetConnectionStringFromKeyVault(_config);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- function_app_entityFramework/Startup.cs
+++ function_app_entityFramework/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using MyClassLibrary.Data;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using MyClassLibrary.UnitOfWork;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace function_app_entityFramework
 {
@@ -12,16 +16,37 @@ namespace function_app_entityFramework
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
             var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
                 .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
             builder.Services.AddDbContext<ToDoListDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            //builder.Services.AddSingleton(typeof(IRepository<,>), typeof(Repository<,>));
+            {
+                options.UseSqlServer(GetConnectionStringFromKeyVault(config));
+            });
 
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+        private static string GetConnectionStringFromKeyVault(IConfiguration config)
+        {
+            try
+            {
+                string keyVaultUrl = config["KeyVaultUrl"];
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                        azureServiceTokenProvider.KeyVaultTokenCallback));
 
-
+                var secret = keyVaultClient.GetSecretAsync(keyVaultUrl, "ConnectionStringa").GetAwaiter().GetResult();
+                return secret.Value;
+            }
+            catch (Exception ex)
+            {
+                // Logga l'eccezione o gestiscila come desideri
+                Console.WriteLine("Errore durante la recupero della connection string dal Key Vault: " + ex.Message);
+                throw;
+            }
         }
     }
 }
